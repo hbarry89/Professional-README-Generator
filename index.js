8ee2414aa5e1c5b8aa19e0a00f3da41e3622e10b
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-15 19:47:17 -0500

    organize code

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+// TODO: Include packages needed for this application
 
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
 
 const questions = [
     {
@@ -67,10 +58,14 @@ const questions = [
       },
 ];
 
+// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('Generating README...'))
 }
+
+// TODO: Create a function to initialize app
 
 function init() {
     inquirer
@@ -81,5 +76,7 @@ function init() {
   }
   );
 }
+
+// Function call to initialize app
 
 init();
